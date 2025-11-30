1059254c402482472da17438918c27adbac44933
Trang <dothuytrang2312@gmail.com>
2025-11-30 15:22:22 +0100

    Complete 3. Mandatory Interpret

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,16 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+// Answer: 2 functions called, line 4 and 5
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+// Answer: Line 5: SyntaxError: missing ) after argument list --> missing a ",", add a coma between "," and """
 
 // c) Identify all the lines that are variable reassignment statements
+// Answer: Line 4 and 5
 
 // d) Identify all the lines that are variable declarations
+// Answer: Line 1, 2, 7, 8
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// Answer: First, to remove all the coma from the carPrice, then change it to number. In sum, this is to change the type of variable from string to integer
--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,10 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+// Answer:
+// 2. const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1): removes the last trailing "p" from the penceString
+// 3. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"): converts the penceString to a format of '###', it means always including 3 digits. If there is only 2 digits, it will be '0##'. If there is 1 digit, it will show '00#' 
+// 4. const pounds = paddedPenceNumberString.substring(0,  paddedPenceNumberString.length - 2): removes the 2 last digits
+// 5. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0"): Takes the last 2 digits, makes sure it will always be 2 digits like '##', if the number is < 10, it will be '0#'
+// 6. console.log(`£${pounds}.${pence}`): print the result after converting to pounds, in this format '£#.##'
