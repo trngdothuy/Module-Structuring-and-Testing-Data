4e31390baec4806718215887af538b0fb6c1efe1
Trang <dothuytrang2312@gmail.com>
2025-12-06 19:06:33 +0100

    Complete 1-key-errors

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// Answer: I think it will cause an error because str is already a parameter so we can't declare again
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -10,4 +11,12 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+// Answer: SyntaxError: Identifier 'str' has already been declared => str is already provided so it cannot be declared here
+
+
 // =============> write your new code here
+// Answer:
+function capitaliseNew(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,6 +2,7 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+// Answer: An error happens because decimalNumber is already provided, cannot be declared again
 
 // Try playing computer with the example to work out what is going on
 
@@ -15,6 +16,15 @@ function convertToPercentage(decimalNumber) {
 console.log(decimalNumber);
 
 // =============> write your explanation here
+// Answer: SyntaxError: Identifier 'decimalNumber' has already been declared -> delete the part of "const" before decimalNumber to avoid declaration again; or we can move this line to be outside of the function. And SyntaxError: Unexpected end of input at the line of console.log(decimalNumber), because decimal value is provided inside the function and cannot be used out of this scope.
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+// Answer: 
+function convertToPercentageNew(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+
+const decimalNumber = 0.5;
+console.log(decimalNumber);
