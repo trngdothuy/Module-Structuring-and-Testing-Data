b78135da32c20925f69767a57144a2b807c30386
Trang <dothuytrang2312@gmail.com>
2025-12-06 22:51:08 +0100

    Complete 2-mandatory-debug

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-
+// Answer: I think it will be an error when running the 2nd console.log
 function multiply(a, b) {
   console.log(a * b);
 }
@@ -9,6 +9,12 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// Answer: Because the function doesnt return anything.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiplyNew(a, b) {
+  return(a * b);
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiplyNew(10, 32)}`);
--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// Answer: It will cause an error when running console.log 
 
 function sum(a, b) {
   return;
@@ -9,5 +10,12 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// Answer: because the function return nothing, the line a + b will never work
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sumNew(a, b) {
+  return(a + b);
+}
+
+console.log(`The sum of 10 and 32 is ${sumNew(10, 32)}`);
